596653ec94736621386ab750f20458a9a9f2ec7b
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-11 21:13:34 +0530

    instance configuration file created

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2Instance" {
     ami = "ami-09cb80360d5069de4"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "vir-key"
     vpc_security_group_ids = [ "secGroup" ]
     tags = {
