29d2361eaf77a93d7024097f6877e207f9931355
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-13 19:19:40 +0530

    instance creation

--- instance.tf
+++ instance.tf
@@ -1,21 +1,33 @@
-# # terraform {
-# #   backend "s3" {
-# #     bucket = "bucketforb92025"
-# #     region = "us-east-1"
-# #     key = "terraform.tfstate"
-# #   }
-# # }
-
-
-
-# resource "aws_instance" "ec2Instance" {
-#     ami = var.ami
-#     instance_type = var.instanceType
-#     key_name = var.key-pair
-#     subnet_id = aws_subnet.pub_subnet.id
-#     security_groups = [aws_security_group.newSecuritygrp.id]
-#     # vpc_security_group_ids = [aws_security_group.newSecuritygrp.id]
-#     tags = {
-#       name = "window-instance"
-#     }
+# terraform {
+#   backend "s3" {
+#     bucket = "bucketforb92025"
+#     region = "us-east-1"
+#     key = "terraform.tfstate"
+#   }
 # }
+
+data "aws_key_pair" "vir-key" {
+    filter {
+      name = "vpc-id"
+      values = [ var.vpc ]
+    }
+    filter {
+      name = "group-name"
+      values = [ "vir-key" ]
+    }
+}
+
+variable "vpc" {
+    default = "vpc-0450ca407485f6731"
+}
+
+resource "aws_instance" "ec2Instance" {
+    ami = var.ami
+    instance_type = var.instanceType
+    key_name = data.aws_key_pair.vir-key.id
+    security_groups = ["secGroup"]
+    # vpc_security_group_ids = [aws_security_group.newSecuritygrp.id]
+    tags = {
+      name = "window-instance"
+    }
+}
