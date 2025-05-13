1877a31bd64a5d049a6e84923181236209a6bb5a
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-13 19:36:46 +0530

    task of vpc

--- instance.tf
+++ instance.tf
@@ -1,26 +1,17 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "bucketforb92025"
-#     region = "us-east-1"
-#     key = "terraform.tfstate"
-#   }
+# # terraform {
+# #   backend "s3" {
+# #     bucket = "bucketforb92025"
+# #     region = "us-east-1"
+# #     key = "terraform.tfstate"
+# #   }
+# # }
+
+# resource "aws_instance" "ec2Instance" {
+#     ami = var.ami
+#     instance_type = var.instanceType
+#     key_name = "vir-key"
+#     security_groups = ["secGroup"]
+#     tags = {
+#       name = "window-instance"
+#     }
 # }
-
-data "aws_key_pair" "vir-key" {
-    key_name = "vir-key"
-}
-
-variable "vpc" {
-    default = "vpc-0450ca407485f6731"
-}
-
-resource "aws_instance" "ec2Instance" {
-    ami = var.ami
-    instance_type = var.instanceType
-    key_name = data.aws_key_pair.vir-key.id
-    security_groups = ["secGroup"]
-    # vpc_security_group_ids = [aws_security_group.newSecuritygrp.id]
-    tags = {
-      name = "window-instance"
-    }
-}
--- variable.tf
+++ variable.tf
@@ -1,14 +1,14 @@
-# variable "vpc_cidr" {
-#     default = "10.0.0.0/16"
-# }
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
 
-# variable "pvt_cidr" {
-#     default = "10.0.0.0/20"
-# }
+variable "pvt_cidr" {
+    default = "10.0.0.0/20"
+}
 
-# variable "pub_cidr" {
-#     default = "10.0.16.0/20"
-# }
+variable "pub_cidr" {
+    default = "10.0.16.0/20"
+}
 
 variable "instanceType" {
     default = "t2.micro"
@@ -18,6 +18,6 @@ variable "ami" {
     default = "ami-0f9de6e2d2f067fca"
 }
 
-# variable "key-pair" {
-#     default = "terra-key"
-# }
+variable "key-pair" {
+    default = "terra-key"
+}
