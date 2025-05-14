eb2d780f776c1b82c965fa8fd72a715166427872
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-14 16:21:20 +0530

    module creation

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
     value = aws_vpc.myvpc.id
 }
+
+output "security_groups_id" {
+    value = aws_security_group.newSecuritygrp.id
+}
+
+output "public_subnet_id" {
+    value = aws_subnet.pub_subnet.id
+}
