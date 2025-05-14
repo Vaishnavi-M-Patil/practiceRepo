3ee9d02f30d83d658848a474d0c39917be21a0fa
Vaishnavi <vaishnavipatil6002@gmail.com>
2025-05-14 16:08:30 +0530

    module creation

--- instance/main.tf
+++ instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ubuntu" {
     ami = var.ami
     instance_type = var.instanceType
     key_name = var.key-pair
-    security_groups = [aws_security_group.newSecuritygrp.id]
+    security_groups = [module.myvpc.aws_security_group.newSecuritygrp.id]
     subnet_id = module.vpc.pub_subnet.id
     tags = {
       name = "Ubuntu Instance"
